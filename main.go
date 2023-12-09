d89de8546780669dc5567e705d8396a7a6a467c2
coffeemug.tester <153386767+coffemugtester@users.noreply.github.com>
2023-12-09 23:24:45 +0100

    fix ports railway

--- main.go
+++ main.go
@@ -3,13 +3,39 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"os"
 )
+
+// Returns PORT from environment if found, defaults to
+// value in `port` parameter otherwise. The returned port
+// is prefixed with a `:`, e.g. `":3000"`.
+func envPortOr(port string) string {
+	// If `PORT` variable in environment exists, return it
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		return ":" + envPort
+	}
+	// Otherwise, return the value of `port` variable from function argument
+	return ":" + port
+}
 
 func main() {
 
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading environmental variables")
+		return
+	}
+
+	// ...
+	// Use `PORT` provided in environment or default to 8080
+	var port = envPortOr("8080")
 	r := gin.Default()
 	SetupRoutes(r)
-	r.Run(":8080")
 
-	fmt.Printf("Server listing on port 8080...")
+	// Listen on all available network interfaces (0.0.0.0) on the specified port
+	if err := r.Run("0.0.0.0" + port); err != nil {
+		fmt.Printf("Error starting the server: %v", err)
+		return
+	}
+
 }
